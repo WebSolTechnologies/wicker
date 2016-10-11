87c40e0038732da31c955fb28be2067dcc864741
Junaid Farooq <junaid@evercam.io>
2016-10-11 19:31:47 +0500

    Added /api/signup route

---
+++ account.js
@@ -0,0 +1,43 @@
+var Firebase = require("firebase");
+var crypto = require("crypto");
+
+function hashPassword(password){
+  return crypto.createHash("sha512").update(password).digest("hex");
+}
+
+var firebase = new Firebase("https://torrid-torch-9503.firebaseio.com/");
+var users = firebase.child("users");
+
+var router = require("express").Router();
+
+router.use(require("body-parser").json());
+router.use(require("cookie-parser")());
+router.use(require("express-session")({
+  resave: false,
+  saveUnintialized: true,
+  secret: "heyfuckoffwhatsup"
+}));
+
+router.post('/api/signup', function(req, res){
+  var username = req.body.username,
+      password = req.body.password;
+
+  if (!username || !password)
+    return res.json({signedIn: false, message: "no username or password!"});
+
+  users.child(username).once("value", function(snapshot){
+    if (snapshot.exists())
+      return res.json({signedIn: false, message: "Username already in use!"});
+
+    var userObj = {
+      username: username,
+      hashPassword: hashPassword
+    };
+    users.child(username).set(userObj);
+    req.session.user = userObj;
+    res.json({
+      signedIn: true,
+      user: userObj;
+    });
+  });
+});
